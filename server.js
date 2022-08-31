44b9e5cbd6a609a2ff71b8ed9b214d856a602c4c
MOHANSURIYA30 <mohansuriya301198@gmail.com>
2022-08-31 15:47:59 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 const adminRoutes = require('./routes/admin');
 const employeeRoutes = require('./routes/employee');
